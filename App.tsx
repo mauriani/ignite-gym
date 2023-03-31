5d81de78b2a9ebd828cf25533e1f8639e1f58510
Mauriani Maciel <maurianim@gmail.com>
2023-03-30 23:20:44 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Text, View, StatusBar } from "react-native";
+import { NativeBaseProvider, Box } from "native-base";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+
+import { THEME } from "./src/theme";
+import { Loading } from "@components/Loading";
+import { SignIn } from "@screens/SignIn";
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+  return (
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar
+        barStyle={"light-content"}
+        backgroundColor="transparent"
+        translucent
+      />
+
+      {fontsLoaded ? <SignIn /> : <Loading />}
+    </NativeBaseProvider>
+  );
+}
---
+++ src/components/Input.tsx
@@ -0,0 +1,17 @@
+import { IInputProps, Input as NativeBaseInput } from "native-base";
+
+export function Input({ ...rest }: IInputProps) {
+  return (
+    <NativeBaseInput
+      bg="gray.700"
+      h={14}
+      px={4}
+      borderWidth={0}
+      fontSize="md"
+      color="white"
+      fontFamily={"body"}
+      mb={4}
+      {...rest}
+    />
+  );
+}
