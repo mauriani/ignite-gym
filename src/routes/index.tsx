5dab37168e918e850eea9cd46658607d96ac903e
mauriani <maurianim@gmail.com>
2023-05-08 17:04:55 -0300

    routes

---
+++ src/routes/index.tsx
@@ -0,0 +1,17 @@
+import { useTheme, Box } from "native-base";
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
+import { AuthRoutes } from "./auth.routes";
+
+export function Routes() {
+  const theme = DefaultTheme;
+  const { colors } = useTheme();
+
+  theme.colors.background = colors.gray[700];
+  return (
+    <Box flex={1} bg="gray.700">
+      <NavigationContainer>
+        <AuthRoutes />
+      </NavigationContainer>
+    </Box>
+  );
+}
