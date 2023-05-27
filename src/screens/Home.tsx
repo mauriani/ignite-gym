7f1d3a57cf7c32f6867b1c91ead77f09baae3b95
mauriani <maurianim@gmail.com>
2023-05-27 15:14:24 -0300

    mudancas em andamento

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { Center, Text, VStack } from "native-base";
+
 import { HomeHeader } from "@components/HomeHeader";
+import { Group } from "@components/Group";
 
 export function Home() {
   return (
     <VStack flex={1}>
       <HomeHeader />
+
+      <Group name="costas" />
     </VStack>
   );
 }
