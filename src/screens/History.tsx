d350cfe121d7ad1c2e9d04f52a055e2e9c734327
mauriani <maurianim@gmail.com>
2023-08-15 16:29:46 -0300

    feat: layout pagina de historico de exercicios

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,10 +1,35 @@
-import React from "react";
-import { Center, Text } from "native-base";
+import React, { useState } from "react";
+import { SectionList, Heading, VStack } from "native-base";
+import { ScreenHeader } from "@components/ScreenHeader";
+import { HistoryCard } from "@components/HistoryCard";
 
 export function History() {
+  const [exercises, setExercises] = useState([
+    {
+      title: "26.08.22",
+      data: ["Costas", "Ombro"],
+    },
+    {
+      title: "6.08.22",
+      data: ["Puxada frontal"],
+    },
+  ]);
+
   return (
-    <Center flex={1}>
-      <Text color="white">History</Text>
-    </Center>
+    <VStack flex={1}>
+      <ScreenHeader title="Histórico de Exercícios" />
+
+      <SectionList
+        sections={exercises}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <HistoryCard />}
+        renderSectionHeader={({ section }) => (
+          <Heading color="gray.200" fontSize={"md"} mt={10} mb={3}>
+            {section.title}
+          </Heading>
+        )}
+        px={8}
+      />
+    </VStack>
   );
 }
